402b304f1e07e50b6a1a652c43191e4a12430cc3
Peter Sherman <pfnsec@gmail.com>
2021-08-12 22:43:45 +0100

    Decode MIDI & slab-allocate messages

--- hardware-eval/nrf_experiment/prog/taiko-hid/src/midi.c
+++ hardware-eval/nrf_experiment/prog/taiko-hid/src/midi.c
@@ -11,14 +11,22 @@
 #include "midi.h"
 
 static K_FIFO_DEFINE(midi_fifo);
+static K_MEM_SLAB_DEFINE(midi_slab, sizeof(midi_message), 64, 4);
+
 
 static struct device *midi_uart_dev;
 
-static midi_message latest_message;
+static midi_message *latest_message = NULL;
+static uint8_t state = MIDI_STATE_IDLE;
 
 static void midi_uart_isr(const struct device *unused, void *user_data) {
 	ARG_UNUSED(unused);
 	ARG_UNUSED(user_data);
+
+    if(latest_message == NULL) {
+        k_mem_slab_alloc(&midi_slab, &latest_message, K_FOREVER);
+        //printk("Realloc message... %p\n", latest_message);
+    }
 
 	if (!(uart_irq_rx_ready(midi_uart_dev) ||
 	      uart_irq_tx_ready(midi_uart_dev))) {
@@ -34,29 +42,58 @@ static void midi_uart_isr(const struct device *unused, void *user_data) {
         static uint8_t chr; 
 
         int rx = uart_fifo_read(midi_uart_dev, &chr, sizeof(chr));
-        if(rx) {
-            latest_message.chr = chr;
-            k_fifo_put(&midi_fifo, &latest_message);
+        if(!rx) {
+            return;
         }
 
-        //LOG_DBG("read %d req %d", rx, len);
-		printk("rx %d, chr %c\n", rx, chr);
-
+        static uint8_t running_status;
+        switch(state) {
+        case MIDI_STATE_IDLE:
+            if(MIDI_IS_STATUS(chr)){
+                if(MIDI_STATUS(chr) == MIDI_NOTE_ON) {
+                    state = MIDI_STATE_NOTE1;
+                    running_status = chr;
+                } else if(MIDI_STATUS(chr) == 0xF) {
+                    // sysex, don't update running_status
+                } else {
+                    running_status = chr;
+                }
+            } else {
+                if(MIDI_STATUS(running_status) == MIDI_NOTE_ON) {
+                    latest_message->note = chr;
+                    state = MIDI_STATE_NOTE2;
+                }
+                //printk("DATA in IDLE! %x\n", chr);
+            }
+            
+            break;
+        case MIDI_STATE_NOTE1:
+            if(MIDI_IS_DATA(chr)){
+                latest_message->note = chr;
+                state = MIDI_STATE_NOTE2;
+            } else {
+                printk("STATUS in NOTE1! \n");
+            }
+            break;
+        case MIDI_STATE_NOTE2:
+            if(MIDI_IS_DATA(chr)){
+                latest_message->velocity = chr;
+                k_fifo_put(&midi_fifo, latest_message);
+                k_mem_slab_alloc(&midi_slab, &latest_message, K_FOREVER);
+                state = MIDI_STATE_IDLE;
+            } else {
+                printk("STATUS in NOTE2! \n");
+            }
+            break;
+        default:
+            printk("STATE CORRUPTED! \n");
+            break;
+        }
 	}
 }
 
 int midi_init() {
 
-    /*
-    #if DT_NODE_HAS_STATUS(MIDI_UART_NODE, okay)
-        printk("I2S enabled in Zephyr DeviceTree \r\n");
-    #else
-        printk("I2S is disabled in DeviceTree\r\n");
-    #endif
-    */
-
-	//midi_uart_dev = DEVICE_DT_GET(MIDI_UART_NODE);
-	//midi_uart_dev = device_get_binding("UART_1");
     midi_uart_dev = device_get_binding(DT_LABEL(DT_NODELABEL(uart1)));
 	if (midi_uart_dev == NULL) {
         printk("midi dev null!");
@@ -80,4 +117,8 @@ int midi_init() {
 
 midi_message *midi_read() {
     return k_fifo_get(&midi_fifo, K_NO_WAIT);
+}
+
+void midi_free(midi_message *message) {
+    k_mem_slab_free(&midi_slab, &message);
 }
--- hardware-eval/nrf_experiment/prog/taiko-hid/src/midi.h
+++ hardware-eval/nrf_experiment/prog/taiko-hid/src/midi.h
@@ -3,14 +3,28 @@
 
 #include <zephyr.h>
 
+#define MIDI_IS_STATUS(x)  (x & (1 << 7))
+#define MIDI_IS_DATA(x)   !(x & (1 << 7))
+#define MIDI_STATUS(x)    ((x & 0xF0) >> 4)
+#define MIDI_INFO(x)       (x & 0x0F)
+#define MIDI_STATUS(x)    ((x & 0xF0) >> 4)
+#define MIDI_INFO(x)       (x & 0x0F)
+#define MIDI_NOTE_ON        0x9
+#define MIDI_NOTE_OFF       0x8
+
+#define MIDI_STATE_IDLE     0x0
+#define MIDI_STATE_NOTE1    0x1
+#define MIDI_STATE_NOTE2    0x2
 
 int midi_init();
 
 typedef struct midi_message {
     void *fifo_reserved;
-    uint8_t chr;
+    uint8_t note;
+    uint8_t velocity;
 } midi_message;
 
 midi_message *midi_read();
+void midi_free(midi_message *message);
 
 #endif
