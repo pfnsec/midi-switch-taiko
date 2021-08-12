4d06bccfa13c5dd9ba3dc612421eab0ab1864ec3
Peter Sherman <pfnsec@gmail.com>
2021-08-12 16:05:41 +0100

    Include midi header

--- hardware-eval/nrf_experiment/prog/taiko-hid/src/midi.h
+++ hardware-eval/nrf_experiment/prog/taiko-hid/src/midi.h
@@ -1,7 +1,16 @@
 #ifndef MIDI_H
 #define MIDI_H
 
+#include <zephyr.h>
+
 
 int midi_init();
+
+typedef struct midi_message {
+    void *fifo_reserved;
+    uint8_t chr;
+} midi_message;
+
+midi_message *midi_read();
 
 #endif
